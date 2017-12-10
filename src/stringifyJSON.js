f20de33f8f181eb807410843a35277f1c1fb784f
Vasyl Nesterenko <vasyl.nrk@gmail.com>
2017-12-09 18:13:09 -0800

    - getElementByClassName - completed - StringifyJSON - 	works for primitive values

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,41 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // string
+
+  if(typeof obj === 'string'){
+    console.log(obj)
+    return '"' + String(obj) + '"';
+  }
+
+  // number
+  if(typeof obj === 'number'){
+    return String(obj);
+  }
+
+  // function
+  if(typeof obj === 'function'){
+    return String(obj);
+  }
+
+  // null
+  if(obj === null){
+    return 'null';
+  }
+
+  // boolean
+  if(typeof obj === 'boolean'){
+    return String(obj);
+  }
+
+  // array
+  if(Array.isArray(obj)){
+    let result = '';
+    for(let i = 0; i < obj.length; i++){
+       result += stringifyJSON(obj[i]);
+    }
+    return ('[' + result + ']');
+  }
+
+  // object
 };
